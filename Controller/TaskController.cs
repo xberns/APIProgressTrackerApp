bfbf0a491e69eca4f532f8083edee80c98c3c59f
BSALINAS <bernadette.salinas2002@gmail.com>
2025-09-28 11:17:23 +0800

    add crud for task contents

--- Controller/TaskController.cs
+++ Controller/TaskController.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using apiprogresstracker.ApplicationDBContext;
 using apiprogresstracker.Model.Tasks;
+using APIProgressTrackerApp.DTO.TaskDTO;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.Extensions.ObjectPool;
 
 namespace apiprogresstracker.Controller
 {
@@ -24,7 +27,6 @@ namespace apiprogresstracker.Controller
         public async Task<ActionResult<TaskTitle>> SaveTitle(TaskTitle datas)
         {
             try
-
             {
                 if (datas == null)
                 {
@@ -52,7 +54,7 @@ namespace apiprogresstracker.Controller
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetTitle(string datas)
+        public async Task<IActionResult> GetTitle(GetTask datas)
         {
             try
             {
@@ -60,7 +62,7 @@ namespace apiprogresstracker.Controller
                 {
                     return BadRequest();
                 }
-                var get = await _context.TaskTitle.Where(x => x.User == datas).ToListAsync();
+                var get = await _context.TaskTitle.Where(x => x.User == datas.User).ToListAsync();
 
                 if (get.Count > 0)
                 {
@@ -73,19 +75,36 @@ namespace apiprogresstracker.Controller
                 return StatusCode(500, ex.Message);
             }
         }
-
         [HttpPost("SaveTaskContent")]
-        public async Task<ActionResult<TaskTitle>> SaveTaskContent(TaskContents datas)
+        public async Task<ActionResult<IEnumerable<TaskContents>>> SaveTaskContent(List<ModifyTaskContent> datas)
         {
             try
-
             {
-                if (datas == null)
+                if (datas == null || datas.Count == 0)
                 {
-                    return BadRequest("Parameter is null.");
+                    return BadRequest("Parameter is null or empty.");
                 }
 
-                await _context.TaskContents.AddAsync(datas);
+                var taskContentsList = new List<TaskContents>();
+
+                foreach (var data in datas)
+                {
+                    var taskContent = new TaskContents
+                    {
+                        Title_id = data.Title_id,
+                        Task_order = data.Task_order,
+                        Task_details = data.Task_details,
+                        Date_created = data.Date_created,
+                        Status = 0,
+                        Status_modified = data.Date_created
+
+                    };
+
+                    taskContentsList.Add(taskContent);
+                }
+
+                await _context.TaskContents.AddRangeAsync(taskContentsList);
+                
                 var saved = await _context.SaveChangesAsync();
 
                 if (saved > 0)
@@ -93,17 +112,18 @@ namespace apiprogresstracker.Controller
                     return Ok(new
                     {
                         message = "Data saved successfully.",
-                        data = datas
+                        data = taskContentsList
                     });
                 }
-                return StatusCode(500, "An unknown error occured while saving the data.");
-            }
 
+                return StatusCode(500, "An unknown error occurred while saving the data.");
+            }
             catch (Exception ex)
             {
                 return StatusCode(500, ex.Message);
             }
         }
+
 
         [HttpGet("GetTaskContent")]
         public async Task<IActionResult> GetTaskContent(int id)
@@ -114,7 +134,9 @@ namespace apiprogresstracker.Controller
                 {
                     return BadRequest();
                 }
-                var get = await _context.TaskContents.Where(x => x.Title_id == id).ToListAsync();
+                var get = await _context.TaskContents
+                          .Where(x => x.Title_id == id)
+                          .OrderBy(x => x.Task_order).ToListAsync();
 
                 if (get.Count > 0)
                 {
@@ -128,25 +150,24 @@ namespace apiprogresstracker.Controller
             }
         }
         [HttpPost("SaveTaskSubContent")]
-        public async Task<ActionResult<TaskTitle>> SaveTaskSubContent(TaskContents datas)
+        public async Task<ActionResult<TaskTitle>> SaveTaskSubContent(TaskContents data)
         {
             try
 
             {
-                if (datas == null)
+                if (data == null)
                 {
                     return BadRequest("Parameter is null.");
                 }
 
-                await _context.TaskContents.AddAsync(datas);
+                await _context.TaskContents.AddAsync(data);
                 var saved = await _context.SaveChangesAsync();
 
                 if (saved > 0)
                 {
                     return Ok(new
                     {
-                        message = "Data saved successfully.",
-                        data = datas
+                        message = "Data saved successfully."
                     });
                 }
                 return StatusCode(500, "An unknown error occured while saving the data.");
@@ -158,29 +179,55 @@ namespace apiprogresstracker.Controller
             }
         }
 
-        [HttpGet("GetTaskSubContent")]
-        public async Task<IActionResult> GetTaskSubContent(int id)
+         [HttpPut("UpdateTasksOrder")]
+        public async Task<ActionResult> UpdatTasksOrder([FromBody] List<UpdateOrder> data)
         {
             try
             {
-                if (id < 0)
-                {
-                    return BadRequest();
-                }
-                var get = await _context.TaskSubContents.Where(x => x.Contents_id == id).ToListAsync();
 
-                if (get.Count > 0)
+                var get = await _context.TaskContents.Where(x => x.Title_id == data[0].Title_id).ToListAsync();
+                var getCount = get.Count;
+                if (data == null)
                 {
-                    return Ok(get);
+                    return StatusCode(404, "Does not exist");
                 }
-                return StatusCode(500, "Error occured during transaction");
+                else
+                {
+                    for (int o = 0; o < getCount; o++)
+                    {
+                        for (int i = 0; i < getCount; i++)
+                        {
+                            if (get[o].Id == data[i].Id)
+                            {
+                                get[o].Task_order = i;
+
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+
+                        }
+                    }
+
+                }
+
+                var saved = await _context.SaveChangesAsync();
+                if (saved > 0)
+                {
+                    return Ok(new
+                    {
+                        message = "Data updated successfully.",
+                    });
+                }
+                 return StatusCode(500, "An unknown error occured while saving the data.");
+
             }
-            catch (Exception ex)
+            catch (DbUpdateConcurrencyException)
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, "A concurrency error occurred.");
             }
         }
-        
-
     }
 }
