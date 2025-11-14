e3d4ab233102f11cd86be821d4d03d2787f20aa6
BSALINAS <bernadette.salinas2002@gmail.com>
2025-11-14 21:04:02 +0800

    add get all needed totals for progress circle with appropriate return values

--- Controller/ProgressCircleController.cs
+++ Controller/ProgressCircleController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using apiprogresstracker.ApplicationDBContext;
+using APIProgressTrackerApp.DTO.TaskDTO;
 namespace APIProgressTrackerApp.Controller
 {
     [ApiController]
@@ -19,11 +20,55 @@ namespace APIProgressTrackerApp.Controller
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetProgress(int id)
+        public async Task<IActionResult> GetProgress([FromQuery] GetTaskContent data)
         {
-            var totalTask = await _context.TaskContents.Where(x => x.Id == id).ToListAsync();
+            try{
+                var task =  await _context.TaskContents
+                            .GroupBy(x => x.Title_id == data.Title_id)
+                            .Select(o => new
+                            {
+                                taskTotal = o.Count(),
+                                taskNew = o.Count(x => x.Status == 0),
+                                taskInProgress = o.Count(x => x.Status == 1),
+                                taskCompleted = o.Count(x => x.Status == 2),
+                                taskOnHold = o.Count(x => x.Status == 3),
+                             }).FirstOrDefaultAsync();
 
-            return StatusCode(200);
+            if (task == null)
+            {
+                 return Ok(new { data = "0", Message = "If existing dats is not showing, please contact the admin." });
+            }
+
+             var subtask = await _context.TaskSubContents
+                            .GroupBy(x => x.Title_id == data.Title_id)
+                            .Select(o => new
+                            {
+                                subtaskTotal = o.Count(),
+                                subtaskNew = o.Count(x => x.Status == 0),
+                                subtaskInProgress = o.Count(x => x.Status == 1),
+                                subtaskCompleted = o.Count(x => x.Status == 2),
+                                subtaskOnHold = o.Count(x => x.Status == 3),
+                             }).FirstOrDefaultAsync();
+          
+            if (subtask == null)
+            {
+                return Ok( new {task, Message = "Loaded succesfully. No subtask"});
+            }
+
+            var total = new
+            {
+                taskTotal = task.taskTotal + subtask.subtaskTotal,
+                taskNew = task.taskNew + subtask.subtaskNew,
+                taskInProgress = task.taskInProgress + subtask.subtaskInProgress,
+                taskCompleted = task.taskCompleted + subtask.subtaskCompleted,
+                taskOnHold = task.taskOnHold + subtask.subtaskOnHold
+            };
+            return Ok(total);
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while fetching.");
+            }
         }
     }
 }
